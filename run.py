2e0da64b185a651c95ed99984f81119ef36ba230
Hemstreet <jonhemstreet@gmail.com>
2016-05-05 13:57:42 -0600

    Updated for basic website implementation

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from battleBot import BattleBot
-from flask import Flask
+from flask import Flask, render_template
 
 bot = BattleBot()
 
@@ -9,19 +9,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_route():
-    return "Home"
+    return render_template('_home.html')
 
 
 @app.route("/attack")
 def attack_route():
     bot.attack()
-    return "Attack"
+
+    return render_template('_attack.html')
 
 
 @app.route("/move/<direction>/<speed>")
 def move_route(direction, speed):
     bot.move(direction, speed)
-    return "Move " + direction + " " + speed
+    return render_template('_move.html',  **locals())
 
 
 if __name__ == "__main__":
